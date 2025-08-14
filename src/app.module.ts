7eb2e140f2a2d90fd8c2f4176b7a15f7d06b6691
Shorazi B. Azizov <ShoraziA@HUMO.local>
2025-08-14 14:17:20 +0500

    add: new res in structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { LoggerMiddleware } from './conseption/middleware'
 import { UsersController } from './users/users.controller'
 import { UsersModule } from './users/users.module'
 import { UsersService } from './users/users.service'
+import { AuthModule } from './auth/auth.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-	imports: [UsersModule],
+	imports: [UsersModule, AuthModule, TicketsModule, ServicesModule],
 	controllers: [UsersController],
 	providers: [UsersService],
 })
